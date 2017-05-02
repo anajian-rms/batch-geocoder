ad3b758e6606ec0d70be072eddf95f67f50bbd5d
Clinton Reece <clintonreece@gmail.com>
2017-05-02 11:19:53 -0700

    Add improved logic for missing addresses

--- batch_geocoder.py
+++ batch_geocoder.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 
 
 def check_auth():
-    """Checks for a Google Maps Geocoding API key and authenticates
+    """Check for a Google Maps Geocoding API key and authenticate.
 
     For authentication to work you must have a Google Maps Geocoding API key
     and the GOOGLE_API_KEY environment variable exported:
@@ -18,21 +18,19 @@ def check_auth():
 
     :rtype: gmaps object (done here so that we only create one Client session)
     """
-
     api_key = os.environ["GOOGLE_API_KEY"]
     gmaps = googlemaps.Client(key=api_key)
     return gmaps
 
 
 def load_addresses(input_file):
-    """Loads addresses from CSV
+    """Load addresses from CSV.
 
     :param input_file: A CSV file with two columns: index, address
     :type input_file: string
 
-    :rtype: address dataframe (so we don't have to reconstruct it later)
+    :rtype: address dataFrame (so we don't have to reconstruct it later)
     """
-
     address_df = pd.read_csv(input_file, usecols=[1], names=['Address'])
     address_list = address_df['Address'].tolist()
     return address_df, address_list
@@ -43,10 +41,10 @@ def geocode_addresses(address_df,
                       initial_address,
                       final_address,
                       gmaps):
-    """Geocodes addresses, adding latitude/longitude columns to dataframe
+    """Geocode addresses, add latitude/longitude columns to dataFrame.
 
-    :param address_df: A dataframe with two columns: index, address
-    :type address_df: dataframe
+    :param address_df: A dataFrame with two columns: index, address
+    :type address_df: dataFrame
 
     :param address_list: A list of addresses to geocode
     :type address_list: list
@@ -61,23 +59,24 @@ def geocode_addresses(address_df,
     address list into a detailed geocoded JSON file
     :type gmaps: gmaps object
 
-    :rtype: address dataframe (contains new Latitude and Longitude columns)
+    :rtype: address dataFrame (contains new Latitude and Longitude columns)
     """
-
     address_list = address_list[initial_address:final_address]
-
+    print address_list
     for address_id, address in enumerate(tqdm(address_list), initial_address):
         geocode_result = []
-        geocode_result = gmaps.geocode(address)
-
+        latitude, longitude = 0, 0
+        # address NaN -> don't geocode
+        if not pd.isnull(address_list[address_id]):
+            geocode_result = gmaps.geocode(address)
+        # geocode_results empty -> latitude, longitude == 0,0
         if geocode_result:
             latitude = geocode_result[0]['geometry']['location']['lat']
             longitude = geocode_result[0]['geometry']['location']['lng']
-
-            address_df.at[address_id, 'Latitude'] = latitude
-            address_df.at[address_id, 'Longitude'] = longitude
-
+        address_df.at[address_id, 'Latitude'] = latitude
+        address_df.at[address_id, 'Longitude'] = longitude
     return address_df
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -93,12 +92,9 @@ if __name__ == '__main__':
                         type=int,
                         help="CSV row to stop geocoding at")
     args = parser.parse_args()
-
     gmaps = check_auth()
-
     input_file = args.i
     data, address_list = load_addresses(input_file)
-
     if args.n:
         final_address = args.n
         initial_address = 0
@@ -110,12 +106,10 @@ if __name__ == '__main__':
         final_address = args.final
     else:
         final_address = len(address_list)
-
     address_df = geocode_addresses(data,
                                    address_list,
                                    initial_address,
                                    final_address,
                                    gmaps)
-
     output_file = args.o
     address_df.to_csv(output_file)
