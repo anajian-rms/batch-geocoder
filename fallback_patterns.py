c52333c1c4e3d3a83a1d6ced9aa31bca9b3724f7
anajian-rms <anajian@rms.com>
2018-09-27 14:41:35 -0700

    'added pattern extraction function'
    Signed-off-by: anajian-rms <anajian@rms.com>

--- fallback_patterns.py
+++ fallback_patterns.py
@@ -1,44 +1,47 @@
+import os
 import sys
+import json
 import pyodbc
 import pandas as pd
-import re
-import json
+import pandas.io.sql
 
 
-CREDENTIALS = r'\config\credentials.json'
-database_name = 'AIG_TEST_EDM'
+CREDENTIALS = 'config\credentials.json'
+DATABASE_NAME = str(sys.argv[1]) if len(sys.argv) > 1 else 'AIG_TEST_EDM'
 
 
 def read_address_table(database):
     """
     This functions reads dbo.Address table from the EDM
     :param database: name of the EDM
-    :return: pandas dataframe
+    :return: df
+    :rtype pandas dataframe
     """
     with open(CREDENTIALS) as f:
-        dbserver = json.load(f)
-        connection_string = 'DRIVER='+dbserver["driver"]+';SERVER='+dbserver["server"]+';DATABASE='+database+';UID='+dbserver["username"]+';PWD='+dbserver["password"]
-        query = 'SELECT * FROM [' + database + '].[dbo].[Address]'
+        dbserver = json.load(f)["MSSQL"]
+        connection_string = 'DRIVER=' + dbserver["driver"] + ';SERVER=' + dbserver["server"] + ';DATABASE=' + database + \
+                            ';UID=' + dbserver["username"] + ';PWD=' + dbserver["password"]
+    query = 'SELECT * FROM [' + database + '].[dbo].[Address]'
 
-    with pyodbc.connect(connection_string) as cnxn:
-        cursor = cnxn.cursor()
-        cursor.execute(query)
-
-    return pd.DataFrame(data=cursor.fetchall())
+    with pyodbc.connect(connection_string) as conn:
+        df = pd.io.sql.read_sql(query, conn)
+    return df
 
 
-def extract_patterns(datafrmae):
+def extract_patterns(dataframe):
     """
-    Thisd function extracts all patterns in the last two parts of
+    This function extracts all patterns in the last two parts of
     the [GeoLocationCode] field from the Address table.
-    :param datafrmae: pandas dataframe version of the Address tyable
-    :return: ???
+    :param dataframe: pandas dataframe version of the Address table
+    :return: table of frequencies of extracted patterns
+    :rtype: pandas series
     """
-    pass
+    seq = dataframe["GeoLocationCode"].apply(lambda x: '-'.join([str(s) for s in x.split('-')[2:]]))
+    return pd.DataFrame(data=Address_df.groupby(seq).size())
 
 
 if __name__ == '__main__':
-
-    database_name = sys.argv[0]
-    df = read_address_table(database=database_name)
-    extract_patterns(df)
+    Address_df = read_address_table(database=DATABASE_NAME)
+    patterns_df = extract_patterns(Address_df)
+    # write results to csv
+    patterns_df.to_csv(os.getcwd()+'\patterns_df.csv', sep=',', header=['Count'])
